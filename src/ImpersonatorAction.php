6c287bd397c52d9eb0070c064273a71571421072
Aryeh Raber <aryeh.raber@gmail.com>
2020-05-03 12:40:56 +0200

    Update Action methods

--- src/ImpersonatorAction.php
+++ src/ImpersonatorAction.php
@@ -10,7 +10,27 @@ class ImpersonatorAction extends Action
 {
     protected static $handle = 'Start Impersonation';
 
-    protected $bulk = false;
+    public function visibleTo($item)
+    {
+        if (! $item instanceof User) {
+            return false;
+        }
+
+        if (Auth::user()->getAuthIdentifier() === $item->getAuthIdentifier()) {
+            return false;
+        }
+
+        if (session()->has('impersonator_id')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function visibleToBulk($items)
+    {
+        return false;
+    }
 
     public function run($items, $values)
     {
@@ -28,23 +48,6 @@ class ImpersonatorAction extends Action
         }
 
         return $user->can('access cp') ? cp_route('dashboard') : '/';
-    }
-
-    public function filter($item)
-    {
-        if (! $item instanceof User) {
-            return false;
-        }
-
-        if (Auth::user()->getAuthIdentifier() === $item->getAuthIdentifier()) {
-            return false;
-        }
-
-        if (session()->has('impersonator_id')) {
-            return false;
-        }
-
-        return true;
     }
 
     public function authorize($user, $item)
